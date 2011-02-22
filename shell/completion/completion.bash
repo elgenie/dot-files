7253bae12ad421f8a18c39a9c9adae9e624a2efc
eletuchy <eugene@fb.com>
2011-02-22 05:25:12 -0800

    Freshen git-completion.bash from the source

--- shell/completion/completion.bash
+++ shell/completion/completion.bash
@@ -1,6 +1,6 @@
 source_one $CONFIG_HOME/completion/ack.bash-completion.sh
 
 # To get a fresh copy: contrib/completion/git-completion.bash
-# wget -O git-completion.bash http://git.kernel.org/?p=git/git.git;a=blob_plain;f=contrib/completion/git-completion.bash;hb=refs/heads/master
+# wget -O git-completion.bash 'http://git.kernel.org/?p=git/git.git;a=blob_plain;f=contrib/completion/git-completion.bash;hb=refs/heads/master'
 source_one $CONFIG_HOME/completion/git-completion.bash
 source_one $CONFIG_HOME/completion/git-completion-$USER.bash
